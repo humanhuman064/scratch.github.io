58b5ee007b40277bc803ea7fe00633309a4e333d
humanhuman064 <114605910+humanhuman064@users.noreply.github.com>
2022-12-27 23:52:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ $('#scratch').wScratchPad({
     size        : 50,          // The size of the brush/scratch.
     bg          : 'index.png',  // Background (image path or hex color).
     fg          : 'canvas.png',  // Foreground (image path or hex color).
-    // bg: '#fff',
-    // fg: '#000',
     realtime    : true,       // Calculates percentage in realitime.
-    // scratchDown : null,       // Set scratchDown callback.
-    // scratchUp   : null,       // Set scratchUp callback.
-    // scratchMove : null,       // Set scratcMove callback.
     cursor      : null, // Set cursor.
     scratchDown: function(e, percent){ console.log(percent); },
     scratchMove: function(e, percent){ console.log(percent); },
